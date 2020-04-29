1fce0d9424377a4dea5c7122ec4bd4f4957dd4cf
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2020-04-29 23:21:34 +0800

    Create posts producer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,9 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        # Publish post data
+        Publisher.publish("posts", @post.attributes)
+        
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
